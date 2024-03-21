ad22c29fa6af6c51cd5f223ddf9a94feec1efa13
Jothi1999 <160332521+Jothi1999@users.noreply.github.com>
2024-03-21 19:36:23 +0530

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,7 @@
+<script>
+        
+        function update()
+        {
+            document.write("Your application was received.We will get back you soon.For other queries please visit the school")
+        }
+    </script>
